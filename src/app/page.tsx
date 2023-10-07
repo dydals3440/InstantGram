41ff4db7a4696b18859bf8a40589ede3ff570d44
김용민 <dydals3440@gmail.com>
2023-10-07 23:05:17 +0900

    feat: Following Bar SWR 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 
+// SSR임, 사용자에게 요청이 올 떄마다, 이 페이지를 만듬. 현재 로그인한 사용자의 정보를 확인해야 하기 떄문 SSG아님. 홈페이지에는 로그인한 사용자가 없다면 의미있는 데이터를 보여주기 힘들기 떄문, 홈페이지 자체적으로는 SSR로 행동. (SideBar는 서버 컴포넌트로)
 export default async function HomePage() {
   // Login User Info
   const session = await getServerSession(authOptions);
@@ -17,6 +18,7 @@ export default async function HomePage() {
   return (
     <section className='w-full flex flex-col md:flex-row max-w-[850px] p-4'>
       <div className='w-full basis-3/4'>
+        {/* 두개의 컴포넌트는 세션에 들어있는 사용자의 정보만으로는 충분하지않음, 누구를 팔로잉하고있는지, 팔로잉하고있는 사람들의 postList를 받아와야함, SSR로 할껀지(서버처리 or 컴포넌트 내부에서 부분적으로 CSR까지 함께) */}
         <FollowingBar />
         <PostList />
       </div>
@@ -26,3 +28,6 @@ export default async function HomePage() {
     </section>
   );
 }
+
+// NAVBAR SIDEBAR 같이 정적인 부분은 SSR로 효울적으로(html을 사용자에게 먼저 보내줌)
+// Following Bar, PostList는 각각 개별적인 데이터를 가져와야하므로, 전체적인 페이지를 SSR로 가져가면 과부하, 이 두개는, CSR이 되도록, 만들어볼 것 (그래도, 정적인 부분들은 프리렌더링 해줄 것)
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,3 +1,17 @@
+'use client';
+
+import useSWR from 'swr';
+
 export default function FollowingBar() {
+  const { data, isLoading, error } = useSWR('/api/hello');
+  console.log(data);
+
+  // 1. 클라이언트 컴포넌트에서 백엔드가 api/me 사용자의 정보를 얻어옴
+  // 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서
+  // 3. 백엔드에서 사용자의 상세 정보를 Sanity에서 가지고 옴(followings)
+  // 4. 여기, 클라이언트 컴포넌트에서 followings의 정보를 UI에 보여줌! (image, username)
+
   return <div>FollowingBar</div>;
 }
+
+// 로그인을 하면, 브라우저 자체적으로 로그인 요청을하고 성공적으로 되면 서버에서 부터 응답헤더에 로그인이 잘되었다는 토(쿠키)을 받아옴, 로그인이 한번 성립이 되면, 그 이후에 클라이언트가 백엔드에 보내는 api 요청에 한해서 헤더에 자동으로 서버에서 받아오는 토큰이 붙어져서 함꼐 보내짐
