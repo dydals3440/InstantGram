ee722739183406b2cf3a8519174c8aa0772c6133
김용민 <dydals3440@gmail.com>
2023-10-17 22:53:37 +0900

    feat: post detail component

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -2,6 +2,10 @@ import { FullPost, SimplePost } from '@/model/post';
 
 import useSWR from 'swr';
 import Image from 'next/image';
+import PostUserAvatar from './PostUserAvatar';
+import ActionBar from './ActionBar';
+import CommentForm from './CommentForm';
+import Avatar from './Avatar';
 
 type Props = {
   post: SimplePost;
@@ -15,7 +19,7 @@ export default function PostDetail({ post }: Props) {
   return (
     <section>
       <div className='relative'>
-        {/* 너비는 지정, 부모는 높이에 따라 */}
+        {/* 너비는 지정, 부모는 높이에 따라 div요소(relative)만큼만 fill */}
         <Image
           src={image}
           alt={`photo by ${username}`}
@@ -23,6 +27,31 @@ export default function PostDetail({ post }: Props) {
           fill
           sizes='650px'
         />
+      </div>
+      <div>
+        <PostUserAvatar image={userImage} username={username} />
+        <ul>
+          {/* post는 정적으로 변하지 않으니까, 배열의 인덱스 사용 */}
+          {comments &&
+            comments.map(
+              ({ image, username: commentUsername, comment }, index) => (
+                <li key={index}>
+                  <Avatar
+                    image={image}
+                    size='small'
+                    // comment를 작성한, userName이, 본인과 일치할때만 하이라이트
+                    highlight={commentUsername === username}
+                  />
+                  <div>
+                    <span>{commentUsername}</span>
+                    <span>{comment}</span>
+                  </div>
+                </li>
+              )
+            )}
+        </ul>
+        <ActionBar likes={likes} username={username} createdAt={createdAt} />
+        <CommentForm />
       </div>
     </section>
   );
