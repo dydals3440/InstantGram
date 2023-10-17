53f474886b537f8afdc5141653a9ec00c38e264f
김용민 <dydals3440@gmail.com>
2023-10-17 22:58:13 +0900

    style: post modal

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -17,10 +17,11 @@ export default function PostDetail({ post }: Props) {
   const comments = data?.comments;
 
   return (
-    <section>
-      <div className='relative'>
+    <section className='flex w-full h-full'>
+      <div className='relative basis-3/5'>
         {/* 너비는 지정, 부모는 높이에 따라 div요소(relative)만큼만 fill */}
         <Image
+          className='object-cover'
           src={image}
           alt={`photo by ${username}`}
           priority
@@ -28,22 +29,22 @@ export default function PostDetail({ post }: Props) {
           sizes='650px'
         />
       </div>
-      <div>
+      <div className='w-full basis-2/5 flex flex-col'>
         <PostUserAvatar image={userImage} username={username} />
-        <ul>
+        <ul className='border-t border-gray-200 h-full overflow-y-auto p-4 mb-1'>
           {/* post는 정적으로 변하지 않으니까, 배열의 인덱스 사용 */}
           {comments &&
             comments.map(
               ({ image, username: commentUsername, comment }, index) => (
-                <li key={index}>
+                <li key={index} className='flex items-center mb-1'>
                   <Avatar
                     image={image}
                     size='small'
                     // comment를 작성한, userName이, 본인과 일치할때만 하이라이트
                     highlight={commentUsername === username}
                   />
-                  <div>
-                    <span>{commentUsername}</span>
+                  <div className='ml-2'>
+                    <span className='font-bold mr-1'>{commentUsername}</span>
                     <span>{comment}</span>
                   </div>
                 </li>
