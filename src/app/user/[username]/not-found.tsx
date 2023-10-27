22dba1e4f5d97158be02d80c38a77dee1ed7004f
김용민 <dydals3440@gmail.com>
2023-10-27 14:53:34 +0900

    feat: UserProfile Service

---
+++ src/app/user/[username]/not-found.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotFound() {
+  return <div>사용자 못찾음 ㅠㅠ</div>;
+}
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,4 +1,4 @@
-export type User = {
+export type AuthUser = {
   name: string;
   username: string;
   email: string;
@@ -6,15 +6,19 @@ export type User = {
 };
 
 // 두개의 정보만 고른다는의미
-export type SimpleUser = Pick<User, 'username' | 'image'>;
+export type SimpleUser = Pick<AuthUser, 'username' | 'image'>;
 
-export type DetailUser = User & {
+export type HomeUser = AuthUser & {
   following: SimpleUser[];
   followers: SimpleUser[];
   bookmarks: string[];
 };
 
-export type ProfileUser = User & {
+export type SearchUser = AuthUser & {
   following: number;
   followers: number;
+};
+
+export type ProfileUser = SearchUser & {
+  posts: number;
 };
