6e2fcd4660cd3dea20f1964adf82043551c9b379
김용민 <dydals3440@gmail.com>
2023-10-28 02:17:36 +0900

    refactor: Heart/Bookmark Icon Props type add

--- src/components/ui/icons/BookmarkIcon.jsx
+++
@@ -1,5 +0,0 @@
-import { BsBookmark } from 'react-icons/bs';
-
-export default function BookmarkIcon() {
-  return <BsBookmark className='h-6 w-6' />;
-}
---
+++ src/components/ui/icons/BookmarkIcon.tsx
@@ -0,0 +1,9 @@
+import { BsBookmark } from 'react-icons/bs';
+
+type Props = {
+  className?: string;
+};
+
+export default function BookmarkIcon({ className }: Props) {
+  return <BsBookmark className={className || 'w-6 h-6'} />;
+}
