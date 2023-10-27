463c073cbe5022e5479f4f8e670b4816061052cf
김용민 <dydals3440@gmail.com>
2023-10-28 02:11:29 +0900

    feat: userPosts service

---
+++ src/app/api/users/[...slug]/route.ts
@@ -0,0 +1,28 @@
+import { getLikedPostsOf, getPostsOf, getSavedPostsOf } from '@/service/posts';
+import { NextRequest, NextResponse } from 'next/server';
+
+type Context = {
+  params: {
+    slug: string[]; // slug/slug/slug 중첩된 라우트 받을 수 있게 만들어줌
+  };
+};
+
+export async function GET(_: NextRequest, context: Context) {
+  const { slug } = context.params;
+
+  if (!slug || !Array.isArray(slug) || slug.length < 2) {
+    return new NextResponse('Bad Request', { status: 400 });
+  }
+
+  const [username, query] = slug;
+
+  let request = getPostsOf;
+
+  if (query === 'saved') {
+    request = getSavedPostsOf;
+  } else if (query === 'liked') {
+    request = getLikedPostsOf;
+  }
+
+  return request(username).then((data) => NextResponse.json(data));
+}
